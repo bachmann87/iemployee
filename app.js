2d00c3122980da204869cde4a93663041824db98
Allan Bachmann <allan.bachmann@yahoo.de>
2018-05-18 23:04:26 +0200

    Added Python Test for Mocha

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ var upload = require('express-fileupload');
 var bodyParser = require('body-parser');
 var chalk = require('chalk');
 var mongoose = require('mongoose');
-// let Vacancie = require('./models/vacancies');
 
 // Core Node Modules
 var fs = require('fs');
 var path = require('path');
 var util = require('util');
 
-// Routers
+// Get App Routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var jobsRouter = require('./routes/jobs');
 
+// Init Express
 var app = express();
 
 // Engine Setup
@@ -29,9 +29,11 @@ app.engine('hbs', hbs({
   layoutsDir: __dirname + '/views/layouts/'
 }));
 
+// Set Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// Set Middleware
 app.use(upload());
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -41,26 +43,29 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set Routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/jobs', jobsRouter);
 
-// catch 404 and forward to error handler
+// Catch 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error Handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error', {
     layout: 'error.hbs'
   });
+  
 });
 
 module.exports = app;
